f221e6a9a7071666a49cc73d47139e760ab74adf
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2021-01-31 19:15:33 -0300

    Embed subProcess

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 using CefSharp;
 using CefSharp.BrowserSubprocess;
+using CefSharp.Internals;
+using CefSharp.RenderProcess;
 using CefSharp.SchemeHandler;
 using CefSharp.WinForms;
 using System;
@@ -18,13 +20,85 @@ namespace NanoLauncher
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-           // Application.SetHighDpiMode(HighDpiMode.SystemAware);
-           //Application.EnableVisualStyles();
-           // Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BrowserForm());
+            if(args.Count() < 5)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new BrowserForm());
+            }
+            else
+            {
+                NanoSubProcess(args);
+            }
 
+        }
+
+        public static int NanoSubProcess(string[] args)
+        {
+            Debug.WriteLine("BrowserSubprocess starting up with command line: " + string.Join("\n", args));
+
+            SubProcess.EnableHighDPISupport();
+
+            int result;
+
+
+            var type = args.GetArgumentValue(CefSharpArguments.SubProcessTypeArgument);
+
+            var parentProcessId = -1;
+
+            // The Crashpad Handler doesn't have any HostProcessIdArgument, so we must not try to
+            // parse it lest we want an ArgumentNullException.
+            if (type != "crashpad-handler")
+            {
+                parentProcessId = int.Parse(args.GetArgumentValue(CefSharpArguments.HostProcessIdArgument));
+                if (args.HasArgument(CefSharpArguments.ExitIfParentProcessClosed))
+                {
+                    Task.Factory.StartNew(() => AwaitParentProcessExit(parentProcessId), TaskCreationOptions.LongRunning);
+                }
+            }
+
+            // Use our custom subProcess provides features like EvaluateJavascript
+            if (type == "renderer")
+            {
+                IRenderProcessHandler handler = null;
+                var wcfEnabled = args.HasArgument(CefSharpArguments.WcfEnabledArgument);
+                var subProcess = wcfEnabled ? new WcfEnabledSubProcess(parentProcessId, handler, args) : new SubProcess(handler, args);
+
+                using (subProcess)
+                {
+                    result = subProcess.Run();
+                }
+            }
+            else
+            {
+                result = SubProcess.ExecuteProcess(args);
+            }
+
+            Debug.WriteLine("BrowserSubprocess shutting down.");
+
+            return result;
+        }
+
+        private static async void AwaitParentProcessExit(int parentProcessId)
+        {
+            try
+            {
+                var parentProcess = Process.GetProcessById(parentProcessId);
+                parentProcess.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                //main process probably died already
+                Debug.WriteLine(e);
+            }
+
+            await Task.Delay(1000); //wait a bit before exiting
+
+            Debug.WriteLine("BrowserSubprocess shutting down forcibly.");
+
+            Environment.Exit(0);
         }
     }
 }
