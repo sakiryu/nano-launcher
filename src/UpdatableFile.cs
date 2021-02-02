9e3b95ee40aa32f7a68696b7df34523a601a48eb
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2021-02-02 11:11:26 -0300

    UpdatablaFiles class to store data regarding files that should be updated(b4 it was stored in a dictionary<string, string>)

---
+++ src/UpdatableFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NanoLauncher
+{
+    public class UpdatableFile
+    {
+        public string Name { get; set; }
+        public string Hash { get; set; }
+    }
+}
